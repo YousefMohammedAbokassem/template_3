e7017c421bba2fb898ed91254ddb551d1b23c3f2
yousef abokassem <youseff2000q@gmail.com>
2022-05-03 03:46:56 +0300

    js edit

--- js/index.js
+++ js/index.js
@@ -1,41 +1,41 @@
-// VanillaTilt.init(document.querySelectorAll(".tilt"), {
-//   max: 20,
-//   speed: 400,
-// });
+VanillaTilt.init(document.querySelectorAll(".tilt"), {
+  max: 20,
+  speed: 400,
+});
 
-// myOtherLinks = document.querySelector(".mega-li");
-// console.log(myOtherLinks);
-// myMega = document.querySelector(".mega-li .menu");
-// console.log(myMega);
-// // myOtherLinks.addEventListener("click", function () {
-// //   myMega.style.opacity = "0";
-// //   myMega.style.top = "142px";
-// // });
+myOtherLinks = document.querySelector(".mega-li");
+console.log(myOtherLinks);
+myMega = document.querySelector(".mega-li .menu");
+console.log(myMega);
 // myOtherLinks.addEventListener("click", function () {
-//   myMega.style.opacity = "1";
-//   myMega.style.top = "74px";
-//   myMega.style.setProperty("z-index", "100");
+//   myMega.style.opacity = "0";
+//   myMega.style.top = "142px";
 // });
-// // myMega.addEventListener("blur", function () {
-// //   console.log("asd");
-// //   myMega.style.display = "none";
-// // });
-// myButton = document.querySelector(".myButton");
-// window.onscroll = function () {
-//   if (window.scrollY >= 500) {
-//     myButton.style.display = "block";
-//   } else {
-//     myButton.style.display = "none";
-//   }
-// };
+myOtherLinks.addEventListener("click", function () {
+  myMega.style.opacity = "1";
+  myMega.style.top = "74px";
+  myMega.style.setProperty("z-index", "100");
+});
+// myMega.addEventListener("blur", function () {
+//   console.log("asd");
+//   myMega.style.display = "none";
+// });
+myButton = document.querySelector(".myButton");
+window.onscroll = function () {
+  if (window.scrollY >= 500) {
+    myButton.style.display = "block";
+  } else {
+    myButton.style.display = "none";
+  }
+};
 
-// myButton.onclick = function () {
-//   scrollTo({
-//     left: 0,
-//     top: 0,
-//     behavior: "smooth",
-//   });
-// };
+myButton.onclick = function () {
+  scrollTo({
+    left: 0,
+    top: 0,
+    behavior: "smooth",
+  });
+};
 
 let timeDown = new Date("Dec 31, 2022 23:59:59").getTime();
 let counter = setInterval(() => {
